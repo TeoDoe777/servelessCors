f3044d860016e8dc7c132192b5fabe51d52dc11b
Teo <turp777@yahoo.com>
2023-12-13 21:35:58 +0400

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,46 +1,33 @@
-addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    /** @type {Request} */
-    const request = event.request;
+module.exports = async (req, res) => {
+ const { method } = req;
+ const { url } = req.query;
 
-    if (request.method !== "GET") {
-      const responseMessage = "This is a GET only cors proxy. See https://github.com/SirJosh3917/cors-get-proxy";
-      
-      return new Response(responseMessage, {
-        status: 405,
-        statusText: "GetOnly"
-      });
-    }
+ if (method !== "GET") {
+   const responseMessage = "This is a GET only cors proxy. See https://github.com/SirJosh3917/cors-get-proxy";
+   
+   return res.status(405).send(responseMessage);
+ }
 
-    const url = new URL(request.url);
-    const parameters = new URLSearchParams(url.search);
-    const targetUrl = parameters.get("url");
+ if (url === undefined) {
+   const responseMessage =
+     "Bad usage! http://cors-get-proxy.sirjosh.workers.dev/?url=<put the url here>\n" +
+     "Repo at https://github.com/SirJosh3917/cors-get-proxy\n" +
+     "2022-05-26 update: Added `cors-get-proxy.sirjosh.workers.dev` to the User-Agent of this service.";
 
-    if (targetUrl === null) {
-      const responseMessage =
-        "Bad usage! http://cors-get-proxy.sirjosh.workers.dev/?url=<put the url here>\n" +
-        "Repo at https://github.com/SirJosh3917/cors-get-proxy\n" +
-        "2022-05-26 update: Added `cors-get-proxy.sirjosh.workers.dev` to the User-Agent of this service.";
+   return res.status(400).send(responseMessage);
+ }
 
-      return new Response(responseMessage, {
-        status: 400
-      });
-    }
+ const response = await fetch(url, {
+   headers: {
+     "User-Agent": "cors-get-proxy.sirjosh.workers.dev",
+   },
+ });
+ const responseBody = await response.text();
 
-    const response = await fetch(targetUrl, {
-      headers: {
-        "User-Agent": "cors-get-proxy.sirjosh.workers.dev",
-      },
-    });
-    const responseBody = await response.text();
+ res.setHeader("Access-Control-Allow-Origin", "*");
+ res.setHeader("Access-Control-Allow-Methods", "GET");
+ res.setHeader("Access-Control-Allow-Headers", "*");
+ res.setHeader("Content-Type", "text/plain");
 
-    return new Response(responseBody, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-ALlow-Headers": "*",
-        "Content-Type": "text/plain"
-      },
-    })
-  })())
-})
+ return res.send(responseBody);
+}
