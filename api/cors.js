03e7b3eb3be805139e99ff905d9fc5c2bd475ead
Teo <turp777@yahoo.com>
2023-12-13 21:30:58 +0400

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,33 +1,46 @@
-export default async function handler(request, response) {
-  
-  const https = require('https');
-  let query = Object.entries(request.query);
-  query.shift();
-  let url = request.query.url;
-  query.forEach(entry => {
-    url += '&' + entry[0] + '=' + entry[1];
-  });
-  
-  
-  const {status,data} = await getRequest(url);
-  
-  response.status(status).send(data);
+addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    /** @type {Request} */
+    const request = event.request;
 
-  function getRequest(url) {
-    
-    return new Promise(resolve => {
-      const req = https.get(url, (resp) => {
-        let data = '';
-        resp.on('data', (chunk) => {
-          data += chunk;
-        });
-        resp.on('end', () => {
-          resolve({
-            status: resp.statusCode,
-            data: data
-          });
-        });
+    if (request.method !== "GET") {
+      const responseMessage = "This is a GET only cors proxy. See https://github.com/SirJosh3917/cors-get-proxy";
+      
+      return new Response(responseMessage, {
+        status: 405,
+        statusText: "GetOnly"
       });
+    }
+
+    const url = new URL(request.url);
+    const parameters = new URLSearchParams(url.search);
+    const targetUrl = parameters.get("url");
+
+    if (targetUrl === null) {
+      const responseMessage =
+        "Bad usage! http://cors-get-proxy.sirjosh.workers.dev/?url=<put the url here>\n" +
+        "Repo at https://github.com/SirJosh3917/cors-get-proxy\n" +
+        "2022-05-26 update: Added `cors-get-proxy.sirjosh.workers.dev` to the User-Agent of this service.";
+
+      return new Response(responseMessage, {
+        status: 400
+      });
+    }
+
+    const response = await fetch(targetUrl, {
+      headers: {
+        "User-Agent": "cors-get-proxy.sirjosh.workers.dev",
+      },
     });
-  }
-}
+    const responseBody = await response.text();
+
+    return new Response(responseBody, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-ALlow-Headers": "*",
+        "Content-Type": "text/plain"
+      },
+    })
+  })())
+})
