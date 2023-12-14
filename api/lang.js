845fb99aa64f2a165a12cc9aea0879480bfed645
Teo <turp777@yahoo.com>
2023-12-14 14:11:33 +0400

    Update lang.js

--- api/lang.js
+++ api/lang.js
@@ -1,7 +1,10 @@
-fetch('https://translate.sn-storage.workers.dev/http:///api/lang?target=en&source=ru&te=2&text=%D1%82%D0%B5%D1%81%D1%82&getraw=true')
-    .then(response => response.text())
-    .then(data => {
-        // Вывод ответа на страницу
-        document.body.innerHTML = data;
-    })
-    .catch(error => console.error('Ошибка:', error));
+module.exports = async (req, res) => {
+    try {
+        const response = await fetch('https://translate.sn-storage.workers.dev/http:///api/lang?target=en&source=ru&te=2&text=%D1%82%D0%B5%D1%81%D1%82&getraw=true');
+        const data = await response.text();
+        res.status(200).send(data);
+    } catch (error) {
+        console.error('Ошибка:', error);
+        res.status(500).send('Произошла ошибка');
+    }
+};
