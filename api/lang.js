e5feaa8fcd06ca5756009e6c5f377d1131e45c7d
Teo <turp777@yahoo.com>
2023-12-14 14:18:06 +0400

    Update lang.js

--- api/lang.js
+++ api/lang.js
@@ -1,6 +1,6 @@
 module.exports = async (req, res) => {
   try {
-    const response = await fetch('https://translate.sn-storage.workers.dev/?target=en&source=ru&te=2&text=пример мир&getraw=true');
+    const response = await fetch('https://worker-bold-math-0d6b.sn-storage.workers.dev/?target=en&source=&te=2&text=привет&getraw=true');
     const data = await response.text();
     res.status(200).send(data);
   } catch (error) {
