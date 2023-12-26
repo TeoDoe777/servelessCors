030de391b824749b4afc8a5e94ae50b6a844418d
Teo <turp777@yahoo.com>
2023-12-26 14:31:06 +0400

    Update lang.js

--- api/lang.js
+++ api/lang.js
@@ -1,5 +1,5 @@
 async function handleRequest(request) {
- const baseUrl = 'https://klondike-gate.vercel.app/api/lang';
+ const baseUrl = 'https://klondike-gate.vercel.app/api/lang'; // Замените на ваш домен
  const fullUrl = new URL(baseUrl + request.url);
  const target = fullUrl.searchParams.get('target');
  const source = fullUrl.searchParams.get('source');
@@ -27,34 +27,25 @@ async function handleRequest(request) {
 
  let translationUrl = 'https://browser.translate.yandex.net/api/v1/tr.json/translate?' + query.toString() + '&text=' + (textArray.map(text => encodeURIComponent(text)).join('&text='))
 
- if (translator === '2') {
-   const content = await translateWithMicrosoftTranslator(source || '', target, textArray)
-   const translations = content.map(item => item.translations[0].text).join('\n')
+ const response = await fetch(translationUrl, {
+   headers: {
+     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.2272 YaBrowser/23.9.0.2272 Yowser/2.5 Safari/537.36'
+   }
+ })
+ const content = await response.json()
+
+ // Удаление "align" из ответа
+ delete content.align;
+
+ if (getRaw === 'true') {
+   const translations = content.text.join('\n');
    return new Response(translations, {
      headers: { 'content-type': 'text/plain' },
    })
  } else {
-   const response = await fetch(translationUrl, {
-     headers: {
-       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.2272 YaBrowser/23.9.0.2272 Yowser/2.5 Safari/537.36'
-     }
+   return new Response(JSON.stringify(content), {
+     headers: { 'content-type': 'application/json' },
    })
-   const content = await response.json()
-
-   // Удаление "align" из ответа
-   delete content.align;
-
-   if (getRaw === 'true') {
-     const translations = content.text.join('\n');
-     return new Response(translations, {
-       headers: { 'content-type': 'text/plain' },
-     })
-   } else {
-     return new Response(JSON.stringify(content), {
-       headers: { 'content-type': 'application/json' },
-     })
-   }
  }
 }
-
 module.exports = handleRequest;
