8f70c9b1c8a7d4ae200b752f8d55dbed1cdd537e
Teo <turp777@yahoo.com>
2024-01-16 22:12:24 +0400

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,33 +1,37 @@
 module.exports = async (req, res) => {
-  const { method } = req;
-  const { url } = req.query;
+ const { method } = req;
+ const { url } = req.query;
 
-  if (!["GET", "POST", "PUT", "PATCH", "DELETE"].includes(method)) {
-      const responseMessage = "";
-      return res.status(405).send(responseMessage);
-  }
+ // Get the origin of the incoming request
+ const origin = req.headers.origin;
 
-  if (url === undefined) {
-      const responseMessage = "Bad usage! /?url=<put the url here>\n";
-      return res.status(400).send(responseMessage);
-  }
+ if (!["GET", "POST", "PUT", "PATCH", "DELETE"].includes(method)) {
+     const responseMessage = "";
+     return res.status(405).send(responseMessage);
+ }
 
-  const body = req.body;
+ if (url === undefined) {
+     const responseMessage = "Bad usage! /?url=<put the url here>\n";
+     return res.status(400).send(responseMessage);
+ }
 
-  const response = await fetch(url, {
-      method: req.method,
-      headers: {
-          "User-Agent": "cors-get-proxy.sirjosh.workers.dev",
-          "Content-Type": req.headers['content-type']
-      },
-      body: body
-  });
+ const body = req.body;
 
-  const responseBody = await response.text();
+ const response = await fetch(url, {
+     method: req.method,
+     headers: {
+         "User-Agent": "cors-get-proxy.sirjosh.workers.dev",
+         "Content-Type": req.headers['content-type']
+     },
+     body: body
+ });
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+ const responseBody = await response.text();
 
-  return res.send(responseBody);
+ // Set the Access-Control-Allow-Origin header to the origin of the incoming request
+ res.setHeader("Access-Control-Allow-Origin", origin);
+ res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+ res.setHeader("Access-Control-Allow-Headers", "*");
+
+ return res.send(responseBody);
 }
