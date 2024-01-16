d3804603a5e92006e0ca791ae6b1b45e7ed6a809
Teo <turp777@yahoo.com>
2024-01-16 22:07:44 +0400

    Update get.js

--- api/get.js
+++ api/get.js
@@ -28,7 +28,6 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Content-Type", "text/plain");
 
   return res.send(responseBody);
 }
